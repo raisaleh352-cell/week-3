2340dfe1ea113a5c017b8746263baa00b6b87f65
raisaleh352-cell <raisaleh352@gmail.com>
2026-03-10 23:14:13 +1300

    Add files via upload

---
+++ # calculate the area of arrowhead.py
@@ -0,0 +1,17 @@
+# area of arrowhead = area of square + area of triangle
+print("Calculating the area of the arrowhead...")
+# total area =area of square(m*u) + area of triangle(n*height/2) 
+def calculate_arrowhead_area(side_length, height):
+# assign values to the variables
+ m= 7
+ u= 9
+ n= 4
+# calculate the area of square
+ area_of_square = 7 * 9 
+ total_square = 63
+# calculate the area of triangle
+ area_of_triangle = 4 * 4 / 2  
+triangle = 8
+# calculate the total area of arrowhead
+total_area = 63 + 8
+print(f"The area of the arrowhead is: {total_area}")
