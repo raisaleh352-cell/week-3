ee0bd17873bc572649a2dee5f1659367c24d3065
raisaleh352-cell <raisaleh352@gmail.com>
2026-03-10 22:38:13 +1300

    Add files via upload

---
+++ # calculate area of composite shapes.py
@@ -0,0 +1,13 @@
+# calculate area of composite shapes
+def calculate_area(shape):
+    total_rectangle_area = s * g
+    cutout_rectangle_area = q * w
+    total_area = total_rectangle_area - cutout_rectangle_area
+# assign values to variables
+s = 10  
+g = 5
+q = 4       
+w = 3
+# calculate and print the area
+area =10 * 5 - 4 * 3
+print("The area of the composite shape is:", area)
