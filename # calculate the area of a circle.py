f6657309fac05d5a9e8d6e30360c8d2197fb138d
raisaleh352-cell <raisaleh352@gmail.com>
2026-03-11 18:24:01 +1300

    Add files via upload

---
+++ # calculate the area of a circle.py
@@ -0,0 +1,12 @@
+# calculate the area of a circle with a cutout semi-circle on the left and a half circle added on the right
+# The added 'half circle' on the right should supposedly match the cutout circle on the left
+# Area of a circle = (A = math.pi * (r ** 2))
+# so half a circle is (A = math.pi * (r ** 2)) / 2
+
+def rectangle_area(base, height):
+    area = (x * y)
+    return area
+x = 6
+y = 8
+print(f"The area for the rectangle with semi-circle cutout is {rectangle_area(x, y)}")
+
